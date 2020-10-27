6c034cf6cff91fa077c466714a563e6442e0ce5c
Eva van Rooijen <evav.rooijen@live.nl>
2020-10-27 10:45:12 +0100

    add data

--- ctf-app.py
+++ ctf-app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title(""" Hello World!""" )
 st.write( """
@@ -6,10 +8,18 @@ Let's check some facts!
 """ )
 
 # Collect data, aggregate number of students
-# Perform RDD
-# Interpret results, write conclusion
-
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
+data = pd.read_csv('wo_inges_ho.csv', delimiter=';')
 st.button("Re-run")
+
+st.title("Exploratory Analysis")
+print(data.describe())
+
+time_series = data.iloc[:,-10:].sum()
+st.line_chart(time_series)
+
+student_growth = np.diff(time_series.values)
+st.line_chart(student_growth)
+
+# Perform RDD
+
+# Interpret results, write conclusion
